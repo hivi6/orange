7e072ceda05f767e10f2cde534170627aa1398ec
hivi6 <89378957+hivi6@users.noreply.github.com>
2026-04-07 21:12:34 +0530

    feat: Add support for usage

--- src/main.c
+++ src/main.c
@@ -1,8 +1,54 @@
 #include "common.h"
 #include "token.h"
 
-int main() {
-	printf("Hello, World!\n");
+// ========================================
+// helper declarations
+// ========================================
+
+void usage(FILE *fptr) {
+	fprintf(fptr, 
+		"USAGE: orange [OPTION] <filepath>\n"
+		"\n"
+		"ABOUT:\n"
+		"    orange is a compiler for a statically typed\n"
+		"    programming language for citrus VM\n"
+		"\n"
+		"OPTION:\n"
+		"    -h, --help  This screen\n");
+}
+
+// ========================================
+// main
+// ========================================
+
+int main(int argc, const char **argv) {
+	int usage_flag = 0;
+
+	// read all the flags
+	int argi = 1;
+	for (; argi < argc; argi++) {
+		if (strcmp(argv[argi], "-h") == 0 || 
+			strcmp(argv[argi], "--help") == 0) {
+			usage_flag = 1;
+		}
+		else {
+			break;
+		}
+	}
+
+	if (usage_flag) {
+		usage(stdout);
+		exit(1);
+	}
+
+	if (argi == argc) {
+		fprintf(stderr, "ERROR: No filepath found\n\n");
+		usage(stderr);
+		exit(1);
+	}
+
+	printf("Filepath: %s\n", argv[argi]);
+
 	return 0;
 }
 
