c013112592e0c1ef850432a6afd9f379e0336f91
hivi6 <89378957+hivi6@users.noreply.github.com>
2026-04-20 23:14:15 +0530

    feat: Add ast for band expr

--- src/ast.c
+++ src/ast.c
@@ -24,6 +24,7 @@ static ast_t *malloc_ast_group_expr(token_t *lparen, ast_t *expr, token_t *rpare
 static ast_t *malloc_ast_binary_expr(ast_t *left, token_t *op, ast_t *right);
 
 static ast_t *expr();
+static ast_t *band_expr();
 static ast_t *equality_expr();
 static ast_t *relation_expr();
 static ast_t *shift_expr();
@@ -178,7 +179,20 @@ static ast_t *malloc_ast_binary_expr(ast_t *left, token_t *op, ast_t *right) {
 }
 
 static ast_t *expr() {
-	return equality_expr();
+	return band_expr();
+}
+
+static ast_t *band_expr() {
+	ast_t *left = equality_expr();
+	while (token_at(0)->kind == TK_AMPERSAND_AMPERSAND) {
+		token_t *op = token_at(0);
+		token_skip(1);
+
+		ast_t *right = equality_expr();
+
+		left = malloc_ast_binary_expr(left, op, right);
+	}
+	return left;
 }
 
 static ast_t *equality_expr() {
