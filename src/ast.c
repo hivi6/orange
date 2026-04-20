d07752b0fd5c77da09c1b5ee755a628db22ad126
hivi6 <89378957+hivi6@users.noreply.github.com>
2026-04-20 23:05:52 +0530

    feat: Add ast for shift expr

--- src/ast.c
+++ src/ast.c
@@ -24,6 +24,7 @@ static ast_t *malloc_ast_group_expr(token_t *lparen, ast_t *expr, token_t *rpare
 static ast_t *malloc_ast_binary_expr(ast_t *left, token_t *op, ast_t *right);
 
 static ast_t *expr();
+static ast_t *shift_expr();
 static ast_t *add_expr();
 static ast_t *mul_expr();
 static ast_t *primary_expr();
@@ -175,7 +176,20 @@ static ast_t *malloc_ast_binary_expr(ast_t *left, token_t *op, ast_t *right) {
 }
 
 static ast_t *expr() {
-	return add_expr();
+	return shift_expr();
+}
+
+static ast_t *shift_expr() {
+	ast_t *left = add_expr();
+	while (token_at(0)->kind == TK_LCHEVRON_LCHEVRON || token_at(0)->kind == TK_RCHEVRON_RCHEVRON) {
+		token_t *op = token_at(0);
+		token_skip(1);
+
+		ast_t *right = add_expr();
+
+		left = malloc_ast_binary_expr(left, op, right);
+	}
+	return left;
 }
 
 static ast_t *add_expr() {
